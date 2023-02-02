a4ae8e45d2555e056f3f339bec3d72b031cd8f49
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2023-02-02 19:33:52 +0530

    Added Post a Property pages unstyles

--- client/src/components/PropertyPosting.js
+++ client/src/components/PropertyPosting.js
@@ -1,5 +1,38 @@
-export default function propertyPosting(){
+import '../css/PropertyPosting.css'
+import { useState } from 'react';
+import PropertyDetails from "../elements/PropertyDetails";
+import AccomodationDetails from "../elements/AccomodationDetails";
+import GalleryDetails from "../elements/GalleryDetails";
+
+export default function PropertyPosting(){
+
+    const [page, setPage] = useState(0)
+
+    let mainComponent
+  
+    if(page === 0){
+      mainComponent = <PropertyDetails nextPage={nextPage}/>
+    }else if(page === 1){
+      mainComponent = <AccomodationDetails nextPage={nextPage}/>
+    }else if(page === 2){
+      mainComponent = <GalleryDetails/>
+    }
+
+    function nextPage() {
+        setPage(prevPage => prevPage + 1)
+    }
+
+
     return(
-        <div className="propertyPosting"></div>
+        <div className="propertyPosting pp-container">
+            <div className="sidebar">
+                <ul className='field-class-list'>
+                    <li className="field-classes" onClick={()=> setPage(0)}>Property Details</li>
+                    <li className="field-classes" onClick={()=> setPage(1)}>Accomodation Details</li>
+                    <li className="field-classes" onClick={()=> setPage(2)}>Gallery Details</li>
+                </ul>
+            </div>
+            {mainComponent}
+        </div>
     )
 }
---
+++ client/src/elements/PropertyListCard.js
@@ -0,0 +1,7 @@
+export default function PropertyListCard(){
+    return (
+        <div>
+            <h1>Property List Card</h1>
+        </div>
+    )
+}
